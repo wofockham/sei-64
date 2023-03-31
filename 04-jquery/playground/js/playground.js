907f887b0ff009e54d3bd389be3bd80bad3a2d04
wofockham <certainthings@gmail.com>
2023-03-31 14:28:20 +1100

    color changing nonsense

--- 04-jquery/playground/js/playground.js
+++ 04-jquery/playground/js/playground.js
@@ -11,4 +11,13 @@ $(document).ready(function () {
     $('#toggle').on('click', function () {
         $('img').fadeToggle(800);
     });
+
+    $('#toggle').on('mousemove', function (event) {
+        const randy = function (max=255) {
+            return Math.round(Math.random() * max);
+        };
+
+        const color = `rgb(${ randy() }, ${ randy()}, ${ randy() })`;
+        $(this).css('background-color', color);
+    });
 });
