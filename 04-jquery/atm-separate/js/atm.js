57146143b777a721b680c79db1ea5fe47302c614
wofockham <certainthings@gmail.com>
2023-04-03 11:54:07 +1000

    ATM with separate UI and business logic

---
+++ 04-jquery/atm-separate/js/atm.js
@@ -0,0 +1,50 @@
+/// This code knows NOTHING about negative balances or overdraft protection.
+
+// It simply connects the DOM elements to logical/business functions in banking.js
+
+// Lots of event handlers and DOM manipulation.
+
+// Our handy render() function updates every single part of the DOM with the latest
+// values from our accounts object.
+const render = function () {
+    $('#checking-balance').text('$' + accounts.checkingBalance);
+    $('#savings-balance').text('$' + accounts.savingsBalance);
+
+    $('.zero').removeClass('zero');
+
+    if (accounts.checkingBalance <= 0) {
+        $('#checking-balance').addClass('zero');
+    }
+
+    if (accounts.savingsBalance <= 0) {
+        $('#savings-balance').addClass('zero');
+    }
+};
+
+$(document).ready(function () {
+    render();
+    
+    $('#checking-deposit').on('click', function () {
+        const amount = $('#checking-amount').val();
+        accounts.checkingDeposit(amount);
+        render();
+    });
+
+    $('#checking-withdraw').on('click', function () {
+        const amount = $('#checking-amount').val();
+        accounts.checkingWithdraw(amount);
+        render();
+    });
+
+    $('#savings-deposit').on('click', function () {
+        const amount = $('#savings-amount').val();
+        accounts.savingsDeposit(amount);
+        render();
+    });
+
+    $('#savings-withdraw').on('click', function () {
+        const amount = $('#savings-amount').val();
+        accounts.savingsWithdraw(amount);
+        render();
+    });
+});
