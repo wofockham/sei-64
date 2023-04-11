ac1c5973c280fd2db2d3a48507984f570441e90e
wofockham <certainthings@gmail.com>
2023-04-11 14:31:59 +1000

    case

--- 05-ruby/conditionals/suburbs.rb
+++ 05-ruby/conditionals/suburbs.rb
@@ -6,15 +6,25 @@ print "In which suburb do you reside: "
 residence = gets.chomp.capitalize
 
 # v2 case
+puts case residence
+when 'Bondi'
+    "Wow beaches etc"
+when 'Glebe'
+    "Nice foreshore walk"
+when 'Roseville'
+    "Lah di dah"
+else
+    "I'm sure that's a very nice place"
+end
 
 # v1 if/elsif
-if residence == 'Bondi'
-    puts "Wow beaches etc"
-elsif residence == 'Glebe'
-    puts "Nice foreshore walk"
-elsif residence == 'Roseville'
-    puts "Lah di dah"
-else
-    puts "I'm sure that's a very nice place"
-end
+# if residence == 'Bondi'
+#     puts "Wow beaches etc"
+# elsif residence == 'Glebe'
+#     puts "Nice foreshore walk"
+# elsif residence == 'Roseville'
+#     puts "Lah di dah"
+# else
+#     puts "I'm sure that's a very nice place"
+# end
 
