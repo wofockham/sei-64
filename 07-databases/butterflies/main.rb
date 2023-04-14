091bb17240d51c1fb230a5262ddda23171275032
wofockham <certainthings@gmail.com>
2023-04-14 15:35:17 +1000

    new create index and show

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -8,9 +8,32 @@ end
 
 # INDEX
 get '/butterflies' do
+    @butterflies = query_db "SELECT * FROM butterflies"
+    erb :butterflies_index
+end
+
+# NEW
+get '/butterflies/new' do
+    erb :butterflies_new
+end
+
+post '/butterflies' do
+    query_db "INSERT INTO butterflies (name, family, image) VALUES ('#{ params[:name] }', '#{ params[:family] }', '#{ params[:image] }')"
+    redirect to('/butterflies') # GET /butterflies
+end
+
+# SHOW
+get '/butterflies/:id' do
+    butterflies = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+    @butterfly = butterflies.first # Extract the single butterfly hash from the array of butterflies
+    erb :butterflies_show
+end
+
+def query_db(sql_statement)
+    puts sql_statement # Optional but nice for debugging
     db = SQLite3::Database.new 'database.sqlite3'
     db.results_as_hash = true
-    @butterflies = db.execute "SELECT * FROM butterflies"
+    results = db.execute sql_statement
     db.close
-    erb :butterflies_index
+    results # implicit return
 end
