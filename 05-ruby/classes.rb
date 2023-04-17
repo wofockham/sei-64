28015a2192fa47b678e1173641c5694578cf485e
wofockham <certainthings@gmail.com>
2023-04-17 15:26:03 +1000

    ruby class inheritance example

--- 05-ruby/classes.rb
+++ 05-ruby/classes.rb
@@ -1,22 +1,42 @@
-class Butterfly < ActiveRecord::Base
-end
-
 # Classes product objects.
 # An object is like a hash -- it has key/value pairs.
 # BUT you have to predefine the keys.
 # A strict hash: you have to predefine the structure or the shape.
 # Also: unlike a Ruby hash, an object can have custom methods (functions inside the object).
 
-# name, instrument, vice
-class MarxBrother
-    # Macro to write getters and setters for you
-    attr_accessor :name, :instrument, :vice
+class Actor
+    def award_speech
+        "I would like to thank my agent and my partner. We did it, baby!"
+    end
+
+    def deny_allegations
+        "I deny that and I don't remember and I was drunk and he's not my type"
+    end
 end
 
-groucho = MarxBrother.new
-groucho.name = 'Groucho'
-groucho.vice = 'cigars'
-groucho.instrument = 'guitar'
-groucho.set_thing = 'whatever'
+class Stooge < Actor
+    def terrible?
+        true
+    end
+end
+
+# name, instrument, vice
+class MarxBrother < Actor
+    # Macro to write getters and setters for you
+    attr_accessor :name, :instrument, :vice
+
+    def initialize(n='', i='', v='lampooning authority figures') # variadic
+        @name = n
+        @instrument = i
+        @vice = v
+    end
+
+    def perform
+        "My name is #{ @name } and I play the #{ @instrument }"
+    end
+end
+
+groucho = MarxBrother.new 'Groucho', 'guitar', 'cigars'
+chico = MarxBrother.new 'Chico', 'piano'
 
 binding.irb
