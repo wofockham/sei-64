d5a4b13f63b9094a448c8a9df9b3a08cb79428e5
wofockham <certainthings@gmail.com>
2023-04-18 21:43:36 +1000

    rails intro ii

--- 08-rails/rails-intro/app/controllers/pages_controller.rb
+++ 08-rails/rails-intro/app/controllers/pages_controller.rb
@@ -6,4 +6,16 @@ class PagesController < ApplicationController
     def goodbye
         render :goodbye
     end
+
+    def funny
+    end
+
+    def helpers
+        @amount = 2132.454349
+        @large_number = 1231232323
+        @phone = 2125551212
+
+        @friend_count = 1
+        @enemy_count = 97
+    end
 end
--- 08-rails/rails-intro/config/routes.rb
+++ 08-rails/rails-intro/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root :to => 'pages#hello'
 
   get '/hello' => 'pages#hello' # 'controller#action'
-  get '/goodbye' => 'pages#goodbye'
+  get '/ciao' => 'pages#goodbye', :as => :goodbye
+  get '/funny' => 'pages#funny'
+  get '/helpers' => 'pages#helpers'
 
   get '/auto/:color' => 'auto#color'
   get '/auto/:hp/:torque' => 'auto#engine'
