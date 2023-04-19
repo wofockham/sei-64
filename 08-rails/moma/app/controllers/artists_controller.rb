5c06669af8a5fefc2b183015d2f1549520aad9ea
wofockham <certainthings@gmail.com>
2023-04-19 15:16:51 +1000

    artists CRUD is finished

--- 08-rails/moma/app/controllers/artists_controller.rb
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -4,11 +4,37 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist = Artist.create artist_params
+    redirect_to artist
   end
 
   def edit
+    @artist = Artist.find params[:id]
+  end
+
+  def update
+    artist = Artist.find params[:id]
+    artist.update artist_params
+    redirect_to artist
   end
 
   def show
+    @artist = Artist.find params[:id]
+  end
+
+  def destroy
+    artist = Artist.find params[:id]
+    artist.destroy
+    redirect_to artists_path
+  end
+
+  private
+  # Strong Params: a technique to sanitise data from the from by ensuring it's on our safe list
+  def artist_params
+    params.require(:artist).permit(:name, :nationality, :dob, :period, :image)
   end
 end
