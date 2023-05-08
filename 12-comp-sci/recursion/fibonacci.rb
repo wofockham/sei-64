6b2fe64f7f32559b0f2dfc3a8ebbf8970c6e8e08
wofockham <certainthings@gmail.com>
2023-05-08 11:33:13 +1000

    fast fibonacci

--- 12-comp-sci/recursion/fibonacci.rb
+++ 12-comp-sci/recursion/fibonacci.rb
@@ -23,8 +23,27 @@ end
 
 # Optional bonus homework for RECURSION LOVERS
 # Make this faster
-
 # Option 1: memoisation
+
+def fib_memo(n)
+    @fib ||= {}
+
+    if @fib[n]
+        @fib[n]
+    elsif (n <= 2)
+        1
+    else
+        @fib[n] = fib_memo(n-1) + fib_memo(n-2)
+    end
+end
+
 # Option 2: iterative recursion
+def fib(n, a=1, b=1)
+    if n == 1 || n == 2 # base case
+        b
+    else
+        fib n-1, b, a+b # Almost mystical
+    end
+end
 
 binding.irb
