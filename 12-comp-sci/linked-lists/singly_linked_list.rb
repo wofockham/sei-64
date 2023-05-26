86fe03c304fc2913dfe5c8ccbb7b7d5a6149bd6e
wofockham <certainthings@gmail.com>
2023-05-26 10:56:46 +1000

    singly linked list with Enumerable

--- 12-comp-sci/linked-lists/singly_linked_list.rb
+++ 12-comp-sci/linked-lists/singly_linked_list.rb
@@ -1,5 +1,6 @@
-
 class SinglyLinkedList
+    include Enumerable # mixin: simulates inheriting from multiple classes
+
     class Node
         attr_accessor :value, :next
     
@@ -37,6 +38,11 @@ class SinglyLinkedList
 
     # TODO: more methods that would be nice
     def remove # remove the first node (opposite of prepend) AKA shift
+        if @head
+            previous_head = @head
+            @head = previous_head.next
+            previous_head.value
+        end
     end
 
     def insert_after(node, new_value)
@@ -47,21 +53,37 @@ class SinglyLinkedList
 
     # Tricky
     def reverse # non-destructive
+        reversed_list = SinglyLinkedList.new
+        current_node = @head
+        while current_node
+            reversed_list.prepend current_node.value
+            current_node = current_node.next
+        end
+        reversed_list
     end
 
     # Trickier
     def reverse! # destructive!
+        @head = self.reverse.head
     end
 
     # Trickiest
     def each # how do execute a block in Ruby?
+        current_node = @head
+        while current_node
+            yield current_node.value if block_given? # execute the do/end block
+            current_node = current_node.next
+        end
+        self
     end
 
     # Bonus: .map, .reduce, .select, .reject, .length AKA .size AKA .count
     # Bonus: bros.at_index(3) # equivalent to bros[3]
 end
 
-bros = SinglyLinkedList.new
-bros = SinglyLinkedList.new 'Groucho'
+bros = SinglyLinkedList.new('Groucho').append('Chico').append('Harpo').append('Gummo')
+bros.each do |name|
+    puts name
+end
 
 binding.irb
