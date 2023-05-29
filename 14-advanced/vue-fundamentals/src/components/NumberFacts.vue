b9bc4cf3f708259a09a49f0b9ccd28e5053f0eef
wofockham <certainthings@gmail.com>
2023-05-29 14:26:33 +1000

    fact clearing

--- 14-advanced/vue-fundamentals/src/components/NumberFacts.vue
+++ 14-advanced/vue-fundamentals/src/components/NumberFacts.vue
@@ -2,7 +2,7 @@
     <div>
         <h2>Numberwang</h2>
         <button @click="fetchFact">Fetch fact</button>
-        <button>Clear facts</button>
+        <button @click="clearFacts">Clear facts</button>
         <p v-for="(fact, i) in facts" :key="i">{{ fact }}</p>
     </div>
 </template>
@@ -21,6 +21,9 @@ export default {
         fetchFact: async function () {
             const { data } = await axios.get('http://numbersapi.com/random/trivia');
             this.facts.push(data);
+        },
+        clearFacts: function () {
+            this.facts = [];
         }
     }
 }
