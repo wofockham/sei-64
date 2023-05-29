92eda41290cc8c54a4bc459517b215b86676c4f1
wofockham <certainthings@gmail.com>
2023-05-29 11:14:44 +1000

    vue fundamentals part i

---
+++ 14-advanced/vue-fundamentals/src/components/ClickrButton.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'ClickrButton',
+    data: function () {
+        // The data method is supposed to return an object with your initial data
+        return {
+            clicks: 0
+        }
+    },
+    methods: {
+        incrementCount: function () {
+            this.clicks++; // Vue can notice mutation
+        }
+    }
+}
+</script>
+
+<template>
+    <button @click="incrementCount">{{ clicks }} clicks so far</button>
+</template>
+
+<style scoped>
+button {
+    background-color: limegreen;
+}
+</style>
---
+++ 14-advanced/vue-fundamentals/src/views/ExperimentsView.vue
@@ -0,0 +1,15 @@
+<script>
+import ClickrButton from '../components/ClickrButton.vue'
+export default {
+    name: 'ExperimentsView',
+    components: {
+        ClickrButton: ClickrButton
+    }
+}
+</script>
+
+<template>
+    <div>
+        <ClickrButton />
+    </div>
+</template>
