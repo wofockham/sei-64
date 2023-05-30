9e6d5d3ec25095dbb82c2a6933482117b912e8f1
wofockham <certainthings@gmail.com>
2023-05-30 10:51:27 +1000

    vue client skellington

---
+++ 14-advanced/vocab-client/src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="app">
+    <div class="ui inverted segment navbar">
+      <div class="ui center aligned container">
+        <div class="ui large secondary inverted pointing menu compact">
+          <RouterLink to="/words" class="item">
+            <i class="comment outline icon" /> Words
+          </RouterLink>
+          <RouterLink to="/words/new" class="item">
+            <i class="plus circle icon" /> New
+          </RouterLink>
+          <RouterLink to="/test" class="item">
+            <i class="graduation cap icon" /> Test
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+
+    <div class="ui text container">
+      <div class="ui one column grid">
+        <div class="column">
+          <RouterView />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#app > div.navbar {
+  margin-bottom: 1.5em;
+}
+
+input {
+  width: 300px;
+}
+
+div.label {
+  width: 120px;
+}
+
+div.input {
+  margin-bottom: 10px;
+}
+
+button.ui.button {
+  margin-top: 15px;
+  display: block;
+}
+</style>
---
+++ 14-advanced/vocab-client/src/main.js
@@ -0,0 +1,11 @@
+import 'semantic-ui-css/semantic.css';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
